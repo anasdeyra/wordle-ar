0f8693e5976e58fbc24cc566dc7448a3eb039403
anasdyera <anasdoura607@gmail.com>
2022-02-14 16:31:49 +0100

    minor style adjustements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Board from "./components/Board.jsx";
+import { Header, InputBox, GameEndModal } from "./components/UI.jsx";
+import words from "./words.json";
 
-function App() {
+function App({ wordle, refreshWordle }) {
+  const [inputText, setInputText] = useState("");
+  const [tries, setTries] = useState(5);
+  const [triesLeft, setTriesLeft] = useState(tries);
+  const [gameBoard, setGameBoard] = useState([]);
+  const [currentRow, setCurrentRow] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  const [gameWon, setGameWon] = useState(false);
+  console.log(wordle);
+
+  function submitWord(word) {
+    var tempgameBoard = gameBoard;
+    for (var i = 0; i < 5; i++) {
+      var index = i + currentRow * 5;
+      if (word[i] === wordle[i]) tempgameBoard[index].state = "bullseye";
+      else if (wordle.includes(word[i])) tempgameBoard[index].state = "match";
+      else tempgameBoard[index].state = "noMatch";
+      tempgameBoard[index].letter = word[i];
+      setGameBoard(tempgameBoard);
+    }
+    if (word === wordle) {
+      setGameOver(true);
+      setGameWon(true);
+      return;
+    }
+    if (triesLeft - 1 === 0) {
+      setGameOver(true);
+      setGameWon(false);
+      return;
+    }
+    setCurrentRow(currentRow + 1);
+    setTriesLeft(triesLeft - 1);
+  }
+
+  function restartGame() {
+    refreshWordle();
+    initBoard();
+    setGameOver(false);
+    setGameWon(false);
+  }
+
+  function filterInput(text) {
+    var filteredText = "";
+    for (var i = 0; i < text.length; i++) {
+      if (!(text.charCodeAt(i) > 1610 || text.charCodeAt(i) < 1575))
+        filteredText += text[i];
+    }
+    return filteredText;
+  }
+
+  function handleInput(e) {
+    setInputText(filterInput(e.target.value));
+  }
+
+  function initBoard() {
+    const initBoardList = [];
+    setTriesLeft(tries);
+    setCurrentRow(0);
+    setInputText("");
+    for (var i = 0; i < tries * 5; i++)
+      initBoardList.push({ id: i, letter: "", state: "empty" });
+    setGameBoard(initBoardList);
+  }
+  useEffect(initBoard, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appContainer">
+      <Header />
+      <Board
+        props={{
+          wordle,
+          triesLeft,
+          setTriesLeft,
+          gameBoard,
+          setGameBoard,
+          currentRow,
+          setCurrentRow,
+        }}
+        tries={tries}
+      />
+      <InputBox
+        onChange={handleInput}
+        placeholder="enter your word"
+        buttonText="Enter"
+        inputText={inputText}
+        submitWord={submitWord}
+        setInputText={setInputText}
+      />
+      {gameOver && (
+        <GameEndModal
+          restartGame={restartGame}
+          hasWon={gameWon}
+          triesNumber={tries - triesLeft}
+        />
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import words from "./words.json";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function WordleGenerator() {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  const [wordle, setWordle] = useState(words[getRandomInt(words.length)]);
+  function refreshWordle() {
+    setWordle(words[getRandomInt(words.length)]);
+  }
+  return (
+    <React.StrictMode>
+      <App refreshWordle={refreshWordle} wordle={wordle} />
+    </React.StrictMode>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<WordleGenerator />, document.getElementById("root"));
